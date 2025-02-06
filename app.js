4151fcb365a9eaa6b7f94aa29185b45037591890
WaleedHassan-cyber <waleedhassan3722@gmail.com>
2025-02-06 15:47:49 +0500

    hell

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 // backend test Routes
 
 app.get('/',(req, res) => {
-    res.status(404).json({ message: "found" });
+    res.status(200).json({ message: "found" });
 });
    
 // Main Routes
